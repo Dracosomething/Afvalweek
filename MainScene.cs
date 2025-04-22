260722138f88852b4b09fedfc4ed9dd7b11d895e
Dracosomething <larsfalk08@gmail.com>
2025-04-22 14:20:33 +0200

    working on trash

--- MainScene.cs
+++ MainScene.cs
@@ -5,8 +5,13 @@ public partial class MainScene : ColorRect
 {
 	public override void _Ready()
 	{
-		Node nodeToAdd = new Node(); // Your node to add
+		Sprite2D nodeToAdd = new Sprite2D(); // Your node to add
 		nodeToAdd.Name = "Test123";
+		Texture2D texture = new Texture2D();
+		texture.ResourcePath = "Screenshot 2024-11-28 174135.png";
+		nodeToAdd.Texture = texture;
+		nodeToAdd.Position = this.Position;
 		AddChild(nodeToAdd);
+		GD.Print(nodeToAdd.GetParent().Name);
 	}
 }
