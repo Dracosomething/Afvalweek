c84706f31ce6c4304a742e06337a6a10e2652a5e
Dracosomething <larsfalk08@gmail.com>
2025-04-23 12:26:07 +0200

    work on mountain generation

--- main.cs
+++ main.cs
@@ -1,11 +1,10 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class main : Node
 {
-	// Member variables here, example:
-	private int _a = 2;
-	private string _b = "textvar";
+	List<string> locations = ["res://art/Modular_Afval_1.png", "res://art/Modular_Afval_2.png", "res://art/Modular_Afval_3.png", "res://art/Modular_Afval_4.png"];
 
 	public override void _Ready()
 	{
@@ -13,11 +12,29 @@ public partial class main : Node
 		// Initialization here.
 		GD.Print("Hello from C# to Godot :)");
 		GD.Print();
+		generateHill(5, this);
 	}
 
 	public override void _Process(double delta)
 	{
 		// Called every frame. Delta is time since the last frame.
 		// Update game logic here.
+	}
+
+	public void generateHill(int itterations, Node scene) {
+		Random random = new Random();
+		for (int i = 0; i <= itterations; i++) {
+			Area2D trash = new Area2D();
+			trash.Name = "trash" + i;
+			TextureRect texture = new TextureRect();
+			Texture2D texture2D = new Texture2D();
+			int index = random.Next(0, 3);
+			texture2D.ResourcePath = locations[index];
+			texture.Texture = texture2D;
+			trash.AddChild(texture);
+			CollisionShape2D collision = new CollisionShape2D();
+			collision.Scale = trash.Scale;
+			trash.AddChild(collision);
+		}
 	}
 }
